590509a2994d4e66d04dbf446433fde2ca84789f
Kuba <kuba@ipor.io>
2024-10-13 12:07:43 +0200

    📗 Copy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,22 @@ function App() {
   const router = useRouter();
 
   return (
-    <div className="max-w-2xl m-auto space-y-8 p-8">
-      <VaultAddressForm
-        onVaultAddressTyped={(address) => {
-          router.push(`/vault/${address}`);
-        }}
-      />
+    <div className="h-[40rem] flex justify-center items-center">
+      <div className="max-w-2xl m-auto space-y-8 p-8">
+        <h1 className="text-2xl font-bold">Next Level DeFi Vault Experience</h1>
+        <p>
+          Check any vault against ERC-4626 standart and execute deposits and
+          withdrawals. Look up vault statistics and breakdown any relevant
+          information you can find in the web.
+        </p>
+        <p>Just type the vault address here:</p>
+
+        <VaultAddressForm
+          onVaultAddressTyped={(address) => {
+            router.push(`/vault/${address}`);
+          }}
+        />
+      </div>
     </div>
   );
 }
